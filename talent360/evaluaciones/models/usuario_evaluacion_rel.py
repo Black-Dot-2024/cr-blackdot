4b456f1c1a74badf9a3acef3e50d2435db67d1a7
dvegaa20 <diego.vega.camacho@gmail.com>
2024-05-08 16:22:47 -0600

    fix(evaluaciones): Send e-mail is now available

--- talent360/evaluaciones/models/usuario_evaluacion_rel.py
+++ talent360/evaluaciones/models/usuario_evaluacion_rel.py
@@ -1,7 +1,6 @@
 from odoo import models, fields, api
 import secrets
 
-
 class UsuarioEvaluacionRel(models.Model):
     _name = "usuario.evaluacion.rel"
     _description = "Relación entre evaluacion y usuarios"
@@ -15,61 +14,52 @@ class UsuarioEvaluacionRel(models.Model):
         ],
         default="pendiente",
     )
-
+    
     # Campos relacionados para acceder a atributos de evaluacion
-    evaluacion_nombre = fields.Char(
-        related="evaluacion_id.nombre", string="Nombre de Evaluación", readonly=True
-    )
-    evaluacion_estado = fields.Selection(
-        related="evaluacion_id.estado", string="Estado de Evaluación", readonly=True
-    )
-    evaluacion_tipo = fields.Selection(
-        related="evaluacion_id.tipo", string="Tipo de Evaluación", readonly=True
-    )
-    evaluacion_usuario_ids = fields.Many2many(
-        related="evaluacion_id.usuario_ids",
-        string="Usuarios de Evaluación",
-        readonly=True,
-    )
+    evaluacion_nombre = fields.Char(related="evaluacion_id.nombre", string="Nombre de Evaluación", readonly=True)
+    evaluacion_estado = fields.Selection(related="evaluacion_id.estado", string="Estado de Evaluación", readonly=True)
+    evaluacion_tipo = fields.Selection(related="evaluacion_id.tipo", string="Tipo de Evaluación", readonly=True)
+    evaluacion_usuario_ids = fields.Many2many(related="evaluacion_id.usuario_ids", string="Usuarios de Evaluación", readonly=True)
     token = fields.Char(string="Token")
-
+    
+    
     def action_get_estado(self, usuario_id, evaluacion_id, token):
         """Método para obtener el estado de la evaluación para el usuario.
-
+        
         :param usuario_id: ID del usuario
         :param evaluacion_id: ID de la evaluación
         :return: estado de la evaluación
         """
         if usuario_id:
             usuario_evaluacion = self.env["usuario.evaluacion.rel"].search(
-                [("usuario_id.id", "=", usuario_id), ("evaluacion_id.id", "=", evaluacion_id)]
+                [("usuario_id", "=", usuario_id), ("evaluacion_id", "=", evaluacion_id)]
             )
         else:
             usuario_evaluacion = self.env["usuario.evaluacion.rel"].search(
-                [("evaluacion_id.id", "=", evaluacion_id), ("token", "=", token)]
+                [("evaluacion_id", "=", evaluacion_id), ("token", "=", token)]
             )
 
         return usuario_evaluacion.contestada
 
     def action_update_estado(self, usuario_id, evaluacion_id, token):
         """Método para actualizar el estado de la evaluación para el usuario.
-
+        
         :param usuario_id: ID del usuario
         :param evaluacion_id: ID de la evaluación
         """
-
+        
         if usuario_id:
             usuario_evaluacion = self.env["usuario.evaluacion.rel"].search(
-                [("usuario_id.id", "=", usuario_id), ("evaluacion_id.id", "=", evaluacion_id)]
+                [("usuario_id", "=", usuario_id), ("evaluacion_id", "=", evaluacion_id)]
             )
         else:
             usuario_evaluacion = self.env["usuario.evaluacion.rel"].search(
-                [("evaluacion_id.id", "=", evaluacion_id), ("token", "=", token)]
+                [("evaluacion_id", "=", evaluacion_id), ("token", "=", token)]
             )
 
         usuario_evaluacion.write({"contestada": "contestada"})
 
-    def enviar_evaluacion_action(self, evaluacion_id):
+    def action_enviar_evaluacion(self, evaluacion_id):
         """
         Ejecuta la acción de redireccionar a la lista de evaluaciones y devuelve un diccionario
 
@@ -84,41 +74,45 @@ class UsuarioEvaluacionRel(models.Model):
         base_url = "http://localhost:8069/evaluacion/responder"
 
         usuario_evaluacion = self.env["usuario.evaluacion.rel"].search(
-            [("evaluacion_id.id", "=", evaluacion_id)]
+            [("evaluacion_id", "=", evaluacion_id)]
         )
 
         for user in usuario_evaluacion:
             token = secrets.token_hex(length)
             if not user.token:
-                user.write({"token": token, "contestada": "pendiente"})
+                user.write({
+                    "token": token,
+                    "contestada": "pendiente"
+                })
 
+                evaluacion_url = f"{base_url}/{evaluacion_id}/{token}"
                 mail_values = {
-                    "subject": "Invitación para completar la evaluación",
-                    "email_from": self.env.user.email_formatted,
-                    "email_to": user.usuario_id.email,
-                    "body_html": f"<p>Hola, <strong>{user.usuario_id.name}</strong></p>"
-                    f"<p>En <strong>{self.env.user.company_id.name}</strong> estamos muy interesados"
-                    f"<p>en conocer tu opinión, a fin de identificar áreas de mejora que nos permitan mejorar</p>"
-                    f"<p>tu experiencia con nosotros. Por ello, te invitamos a responder la Encuesta de Clima</p>"
-                    f"<p>Laboral: <strong>(Nombre de evaluación)</strong></p>"
-                    f"<p>Disponible del <strong>(Fecha Inicio)</strong> al <strong>(Fecha Fin)</strong></p>"
-                    f'<a href="{base_url}/{evaluacion_id}/{token}">',
+                    'subject': 'Invitación para completar la evaluación',
+                    'email_from': "talent360@cr-organizacional.com",
+                    'email_to': user.usuario_id.email,
+                    'body_html': 
+                        f'''<p>Hola, <strong>{user.usuario_id.name}</strong>,</p>
+                        <p>En <strong>{self.env.user.company_id.name}</strong> estamos interesados en tu opinión para mejorar.</p>
+                        <p>Por favor, participa en la evaluación de clima laboral disponible del <strong>(Fecha Inicio)</strong> al <strong>(Fecha Fin)</strong>.</p>
+                        <p>Puedes comenzar la evaluación haciendo clic en el siguiente enlace:</p>
+                        <p><a href="{evaluacion_url}">Comenzar Evaluación</a></p>''',
                 }
 
-                mail = self.env["mail.mail"].create(mail_values)
-                # mail.send()
+                mail = self.env['mail.mail'].create(mail_values)
+                mail.send()
 
-                if mail.state == "sent":
+                if mail.state == 'sent':
                     print(f"Correo enviado exitosamente a {user.usuario_id.email}")
-                elif mail.state == "exception":
+                elif mail.state == 'exception':
                     print(f"Fallo al enviar correo a {user.usuario_id.email}")
                 else:
                     print(f"Correo en estado pendiente o desconocido: {mail.state}")
-
-        # return {
-        #     "type": "ir.actions.act_window",
-        #     "name": "Evaluaciones",
-        #     "res_model": "evaluacion",
-        #     "view_mode": "tree",
-        #     "target": "current",
-        # }
+    
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Evaluaciones",
+            "res_model": "evaluacion.evaluacion",
+            "view_mode": "tree,form",
+            "target": "current",
+        }    
+    
